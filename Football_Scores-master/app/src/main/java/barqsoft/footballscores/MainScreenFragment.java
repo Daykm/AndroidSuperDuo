f92bd2d17297a7e55a45cf10226289219f0898b6
daykm <daykmsc@gmail.com>
2015-10-19 09:34:38 -0400

    asdfsadf

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
@@ -13,46 +13,74 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import barqsoft.footballscores.service.myFetchService;
+import barqsoft.footballscores.service.FetchService;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainScreenFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>
-{
-    public scoresAdapter mAdapter;
+public class MainScreenFragment extends Fragment {
+    public ScoresAdapter mAdapter;
     public static final int SCORES_LOADER = 0;
     private String[] fragmentdate = new String[1];
     private int last_selected_item = -1;
 
-    public MainScreenFragment()
-    {
+    public MainScreenFragment() {
     }
 
-    private void update_scores()
-    {
-        Intent service_start = new Intent(getActivity(), myFetchService.class);
+    private void update_scores() {
+        Intent service_start = new Intent(getActivity(), FetchService.class);
         getActivity().startService(service_start);
     }
-    public void setFragmentDate(String date)
-    {
+
+    public void setFragmentDate(String date) {
         fragmentdate[0] = date;
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
         update_scores();
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         final ListView score_list = (ListView) rootView.findViewById(R.id.scores_list);
-        mAdapter = new scoresAdapter(getActivity(),null,0);
+        mAdapter = new ScoresAdapter(getActivity(), null, 0);
         score_list.setAdapter(mAdapter);
-        getLoaderManager().initLoader(SCORES_LOADER,null,this);
+        LoaderManager.LoaderCallbacks<Cursor> callbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                return new CursorLoader(getContext(), DatabaseContract.scores_table.buildScoreWithDate(),
+                        null, null, fragmentdate, null);
+            }
+
+            @Override public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+                //Log.v(FetchScoreTask.LOG_TAG,"loader finished");
+                //cursor.moveToFirst();
+                /*
+                while (!cursor.isAfterLast()) {
+                    Log.v(FetchScoreTask.LOG_TAG, cursor.getString(1));
+                    cursor.moveToNext();
+                }
+                */
+                /*
+                int i = 0;
+                cursor.moveToFirst();
+                while (!cursor.isAfterLast()) {
+                    i++;
+                    cursor.moveToNext();
+                }
+                */
+                //Log.v(FetchScoreTask.LOG_TAG,"Loader query: " + String.valueOf(i));
+                mAdapter.swapCursor(cursor);
+                //mAdapter.notifyDataSetChanged();
+            }
+
+            @Override public void onLoaderReset(Loader<Cursor> loader) {
+                mAdapter.swapCursor(null);
+            }
+        };
+        getLoaderManager().initLoader(SCORES_LOADER, null, callbacks);
         mAdapter.detail_match_id = MainActivity.selected_match_id;
-        score_list.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        score_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ViewHolder selected = (ViewHolder) view.getTag();
                 mAdapter.detail_match_id = selected.match_id;
                 MainActivity.selected_match_id = (int) selected.match_id;
@@ -61,44 +89,4 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
         });
         return rootView;
     }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
-    {
-        return new CursorLoader(getActivity(),DatabaseContract.scores_table.buildScoreWithDate(),
-                null,null,fragmentdate,null);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
-    {
-        //Log.v(FetchScoreTask.LOG_TAG,"loader finished");
-        //cursor.moveToFirst();
-        /*
-        while (!cursor.isAfterLast())
-        {
-            Log.v(FetchScoreTask.LOG_TAG,cursor.getString(1));
-            cursor.moveToNext();
-        }
-        */
-
-        int i = 0;
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast())
-        {
-            i++;
-            cursor.moveToNext();
-        }
-        //Log.v(FetchScoreTask.LOG_TAG,"Loader query: " + String.valueOf(i));
-        mAdapter.swapCursor(cursor);
-        //mAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader)
-    {
-        mAdapter.swapCursor(null);
-    }
-
-
 }
--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,78 +20,62 @@ import java.util.Date;
 /**
  * Created by yehya khaled on 2/27/2015.
  */
-public class PagerFragment extends Fragment
-{
+public class PagerFragment extends Fragment {
     public static final int NUM_PAGES = 5;
     public ViewPager mPagerHandler;
     private myPageAdapter mPagerAdapter;
     private MainScreenFragment[] viewFragments = new MainScreenFragment[5];
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
         mPagerHandler = (ViewPager) rootView.findViewById(R.id.pager);
-        mPagerAdapter = new myPageAdapter(getChildFragmentManager());
-        for (int i = 0;i < NUM_PAGES;i++)
-        {
-            Date fragmentdate = new Date(System.currentTimeMillis()+((i-2)*86400000));
-            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
-            viewFragments[i] = new MainScreenFragment();
-            viewFragments[i].setFragmentDate(mformat.format(fragmentdate));
-        }
         mPagerHandler.setAdapter(mPagerAdapter);
+        for (int i = 0; i < NUM_PAGES; i++) {
+            Date fragmentdate = new Date(System.currentTimeMillis() + ((i - 2) * 86400000));
+            viewFragments[i] = new MainScreenFragment();
+            viewFragments[i].setFragmentDate(SimpleDateFormat.getDateInstance().format(fragmentdate));
+        }
+        mPagerAdapter = new myPageAdapter(getChildFragmentManager());
         mPagerHandler.setCurrentItem(MainActivity.current_fragment);
         return rootView;
     }
-    private class myPageAdapter extends FragmentStatePagerAdapter
-    {
+
+    private class myPageAdapter extends FragmentStatePagerAdapter {
         @Override
-        public Fragment getItem(int i)
-        {
+        public Fragment getItem(int i) {
             return viewFragments[i];
         }
 
         @Override
-        public int getCount()
-        {
+        public int getCount() {
             return NUM_PAGES;
         }
 
-        public myPageAdapter(FragmentManager fm)
-        {
+        public myPageAdapter(FragmentManager fm) {
             super(fm);
         }
+
         // Returns the page title for the top indicator
         @Override
-        public CharSequence getPageTitle(int position)
-        {
-            return getDayName(getActivity(),System.currentTimeMillis()+((position-2)*86400000));
+        public CharSequence getPageTitle(int position) {
+            return getDayName(getContext(), System.currentTimeMillis() + ((position - 2) * 86400000));
         }
+
         public String getDayName(Context context, long dateInMillis) {
             // If the date is today, return the localized version of "Today" instead of the actual
             // day name.
-
-            Time t = new Time();
-            t.setToNow();
-            int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
-            int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
-            if (julianDay == currentJulianDay) {
+            LocalDate date = new LocalDate(dateInMillis);
+            LocalDate today = new LocalDate();
+            if (date.getDayOfWeek() == today.getDayOfWeek()) {
                 return context.getString(R.string.today);
-            } else if ( julianDay == currentJulianDay +1 ) {
+            } else if (date.getDayOfWeek() + 1 == today.getDayOfWeek()) {
+                return context.getString(R.string.yesterday);
+            } else if(date.getDayOfWeek() - 1 == today.getDayOfWeek()) {
                 return context.getString(R.string.tomorrow);
             }
-             else if ( julianDay == currentJulianDay -1)
-            {
-                return context.getString(R.string.yesterday);
-            }
-            else
-            {
-                Time time = new Time();
-                time.setToNow();
-                // Otherwise, the format is just the day of the week (e.g "Wednesday".
-                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
-                return dayFormat.format(dateInMillis);
-            }
+
+            return date.toString(DateTimeFormat.forPattern("EEEE"));
         }
     }
 }
